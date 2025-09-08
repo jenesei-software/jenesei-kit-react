4965ac023fe8fa503d07450787931ef03b10dc8e
CyrilStrone <CyrilStrone@users.noreply.github.com>
2025-09-08 12:19:31 +0000

    deploy: 2c0d516d16747191db93fd8f79db203545c2d181

---
+++ assets/component-image-button.stories-DVE3EUSi.js
@@ -0,0 +1,40 @@
+import{p as l}from"./iframe-CsqTgGki.js";const s={component:l,title:"Component/Image/Button"},e={args:{button:{genre:"blackBorder",size:"medium",children:"Add Image"},dialog:{button:{genre:"blackBorder",size:"medium"},buttonDelete:{genre:"blackBorder",size:"medium"}},locale:{buttonAdd:"Add Image",dialogSave:"Save",dialogCancel:"Cancel",dialogAddImage:"Add",dialogDeleteImage:"Delete"},imageSettings:{aspect:2/2,maxCount:1,maxSize:5*1024*1024},onSave(n){n?n.forEach(a=>{console.log(`Added file: ${a.name}, size: ${a.isNew} bytes`)}):console.log("No files added")}}};var o,d,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    button: {
+      genre: 'blackBorder',
+      size: 'medium',
+      children: 'Add Image'
+    },
+    dialog: {
+      button: {
+        genre: 'blackBorder',
+        size: 'medium'
+      },
+      buttonDelete: {
+        genre: 'blackBorder',
+        size: 'medium'
+      }
+    },
+    locale: {
+      buttonAdd: 'Add Image',
+      dialogSave: 'Save',
+      dialogCancel: 'Cancel',
+      dialogAddImage: 'Add',
+      dialogDeleteImage: 'Delete'
+    },
+    imageSettings: {
+      aspect: 2 / 2,
+      maxCount: 1,
+      maxSize: 5 * 1024 * 1024 // 5 MB
+    },
+    onSave(files) {
+      if (files) {
+        files.forEach(file => {
+          console.log(\`Added file: \${file.name}, size: \${file.isNew} bytes\`);
+        });
+      } else {
+        console.log('No files added');
+      }
+    }
+  }
+}`,...(t=(d=e.parameters)==null?void 0:d.docs)==null?void 0:t.source}}};const r=["Button"];export{e as Button,r as __namedExportsOrder,s as default};
---
+++ assets/component-image.stories-D7tpwLxx.js
@@ -0,0 +1,13 @@
+import{t as o}from"./iframe-CsqTgGki.js";const l={component:o,title:"Component/Image/Image"},e={args:{src:"https://wallpapercat.com/w/full/1/d/5/1412415-3840x2160-desktop-4k-los-angeles-skyline-wallpaper-photo.jpg",alt:"Los Angeles",sxStack:{default:{width:"100%",height:"100%",borderRadius:"12px"}}}};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    src: 'https://wallpapercat.com/w/full/1/d/5/1412415-3840x2160-desktop-4k-los-angeles-skyline-wallpaper-photo.jpg',
+    alt: 'Los Angeles',
+    sxStack: {
+      default: {
+        width: '100%',
+        height: '100%',
+        borderRadius: '12px'
+      }
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const n=["Image"];export{e as Image,n as __namedExportsOrder,l as default};
