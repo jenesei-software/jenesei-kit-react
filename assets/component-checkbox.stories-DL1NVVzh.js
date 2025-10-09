6e9b8d7a4aad6fe4c410ee6adec8996b322c15ff
CyrilStrone <CyrilStrone@users.noreply.github.com>
2025-10-09 16:46:29 +0000

    deploy: df80994f5b8547bd37efde499ae63a0f9024ea56

---
+++ assets/component-checkbox.stories-DL1NVVzh.js
@@ -0,0 +1,23 @@
+import{h as c,j as i,r as s}from"./iframe-PmyQk7IH.js";import"./preload-helper-C1FmrZbK.js";const k={component:c,title:"Component/Checkbox"},u=e=>{const[d,n]=s.useState(e.checked??!1);return s.useEffect(()=>{n(e.checked??!1)},[e.checked]),i.jsxDEV(c,{...e,checked:d,onChange:l=>n(l)},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/component-checkbox.stories.tsx",lineNumber:15,columnNumber:10},void 0)},r={render:e=>i.jsxDEV(u,{...e},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/component-checkbox.stories.tsx",lineNumber:18,columnNumber:19},void 0),args:{genre:"gray",view:{true:{type:"checkbox",name:"Square-true"},false:{type:"checkbox",name:"Square-false"}},checked:!0,children:"First",size:"medium",isLoading:!1,isDisabled:!1,isOnlyLoading:!1,isHiddenBorder:!1}};var a,o,t;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => <CheckboxWrapper {...args} />,
+  args: {
+    genre: 'gray',
+    view: {
+      true: {
+        type: 'checkbox',
+        name: 'Square-true'
+      },
+      false: {
+        type: 'checkbox',
+        name: 'Square-false'
+      }
+    },
+    checked: true,
+    children: 'First',
+    size: 'medium',
+    isLoading: false,
+    isDisabled: false,
+    isOnlyLoading: false,
+    isHiddenBorder: false
+  }
+}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const h=["Checkbox"];export{r as Checkbox,h as __namedExportsOrder,k as default};
---
+++ assets/context-app.stories-COG50ynA.js
@@ -0,0 +1,143 @@
+import{W as l,j as e,b as t,S as v}from"./iframe-PmyQk7IH.js";import"./preload-helper-C1FmrZbK.js";const A={component:l,title:"Context/App"},w=e.jsxDEV("div",{style:{backgroundColor:"darkgoldenrod",padding:"2px",height:"100%",width:"100%"},children:e.jsxDEV(t,{sx:{default:{variant:"h8",wrap:"nowrap"}},children:"Notification"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:18,columnNumber:5},void 0)},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:12,columnNumber:29},void 0),a=e.jsxDEV("div",{style:{backgroundColor:"lightblue",padding:"10px",height:"100%",width:"100%"},children:e.jsxDEV(t,{sx:{default:{variant:"h5",wrap:"nowrap"}},children:"Header"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:33,columnNumber:5},void 0)},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:27,columnNumber:23},void 0),s=e.jsxDEV("div",{style:{backgroundColor:"lightcoral",padding:"10px",height:"100%",width:"100%"},children:e.jsxDEV(t,{sx:{default:{variant:"h5"}},children:"Footer"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:48,columnNumber:5},void 0)},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:42,columnNumber:23},void 0),g=e.jsxDEV("div",{style:{backgroundColor:"lightgreen",padding:"10px",height:"100%",width:"100%"},children:e.jsxDEV(t,{sx:{default:{variant:"h6"}},children:"Left Aside"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:62,columnNumber:5},void 0)},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:56,columnNumber:26},void 0),N=e.jsxDEV("div",{style:{backgroundColor:"lightyellow",padding:"10px",height:"100%",width:"100%"},children:e.jsxDEV(t,{sx:{default:{variant:"h6"}},children:"Right Aside"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:76,columnNumber:5},void 0)},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:70,columnNumber:27},void 0),j=e.jsxDEV("div",{style:{backgroundColor:"beige",padding:"10px",height:"100%",width:"100%"},children:e.jsxDEV(t,{sx:{default:{variant:"h6"}},children:"Nav"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:90,columnNumber:5},void 0)},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:84,columnNumber:20},void 0),d=e.jsxDEV(v,{sx:n=>({default:{backgroundColor:n.palette.whiteStandard,padding:"10px",flexDirection:"column",height:"fit-content"},tablet:{backgroundColor:n.palette.whiteJanice,padding:"10px"},mobile:{backgroundColor:n.palette.grayMonica,padding:"10px"}}),children:[e.jsxDEV(t,{sx:{default:{variant:"h1"}},children:"Title H1"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:114,columnNumber:5},void 0),e.jsxDEV(t,{sx:{default:{variant:"h2"}},children:"Title H2"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:121,columnNumber:5},void 0),e.jsxDEV(t,{sx:{default:{variant:"h3"}},children:"Title H3"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:128,columnNumber:5},void 0),e.jsxDEV(t,{sx:{default:{variant:"h4"}},children:"Title H4"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:135,columnNumber:5},void 0),e.jsxDEV(t,{sx:{default:{variant:"h5"}},children:"Title H5"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:142,columnNumber:5},void 0),e.jsxDEV(t,{sx:{default:{variant:"h6"}},children:"Title H6"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:149,columnNumber:5},void 0)]},void 0,!0,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:98,columnNumber:25},void 0),o={args:{defaultPreview:{defaultVisible:!1,visible:!0,minTime:2e3,content:e.jsxDEV(t,{sx:{default:{variant:"h6",weight:700}},children:"Loading"},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:163,columnNumber:16},void 0)},defaultBgColor:"whiteStandard",isScrollOutlet:!1,defaultTitle:"Storybook?",defaultDescription:"Description from Storybook?",notification:{component:w,length:{default:"20px",tablet:"20px",mobile:null}},header:{component:a,length:{default:"80px",tablet:"60px",mobile:"40px"}},leftAside:{component:g,length:{default:"180px",tablet:"160px",mobile:null}},nav:{component:j,length:{default:"80px",tablet:"60px",mobile:"40px"}},rightAside:{component:N,length:{default:"180px",tablet:"160px",mobile:null}},footer:{component:s,length:{default:"80px",tablet:"60px",mobile:"40px"}},children:d}},i={render:n=>e.jsxDEV(l,{...n},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:228,columnNumber:19},void 0),args:{defaultBgColor:"black40",isScrollOutlet:!0,header:{component:a,length:{default:"80px",tablet:"60px",mobile:"40px"}},nav:{component:j,length:{default:"80px",tablet:"60px",mobile:"40px"}},leftAside:{component:g,length:{default:"80px",tablet:"60px",mobile:null},isTopNav:!0},rightAside:{component:N,length:{default:"80px",tablet:"60px",mobile:null},isTopFooter:!0,isTopHeader:!0},footer:{component:s,length:{default:"80px",tablet:"60px",mobile:"40px"}},children:d}},r={render:n=>e.jsxDEV(l,{...n},void 0,!1,{fileName:"/home/runner/work/jenesei-kit-react/jenesei-kit-react/.storybook-stories/default/context-app.stories.tsx",lineNumber:279,columnNumber:19},void 0),args:{defaultBgColor:"black50",isScrollOutlet:!1,header:{component:a,length:{default:"80px",tablet:"60px",mobile:"40px"}},footer:{component:s,length:{default:"80px",tablet:"60px",mobile:null}},children:d}};var u,c,p;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    defaultPreview: {
+      defaultVisible: false,
+      visible: true,
+      minTime: 2000,
+      content: <Typography sx={{
+        default: {
+          variant: 'h6',
+          weight: 700
+        }
+      }}>
+          Loading
+        </Typography>
+    },
+    defaultBgColor: 'whiteStandard',
+    isScrollOutlet: false,
+    defaultTitle: 'Storybook?',
+    defaultDescription: 'Description from Storybook?',
+    notification: {
+      component: defaultNotification,
+      length: {
+        default: '20px',
+        tablet: '20px',
+        mobile: null
+      }
+    },
+    header: {
+      component: defaultHeader,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: '40px'
+      }
+    },
+    leftAside: {
+      component: defaultLeftAside,
+      length: {
+        default: '180px',
+        tablet: '160px',
+        mobile: null
+      }
+    },
+    nav: {
+      component: defaultNav,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: '40px'
+      }
+    },
+    rightAside: {
+      component: defaultRightAside,
+      length: {
+        default: '180px',
+        tablet: '160px',
+        mobile: null
+      }
+    },
+    footer: {
+      component: defaultFooter,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: '40px'
+      }
+    },
+    children: defaultChildren
+  }
+}`,...(p=(c=o.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var f,m,x;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => <ProviderApp {...args} />,
+  args: {
+    defaultBgColor: 'black40',
+    isScrollOutlet: true,
+    header: {
+      component: defaultHeader,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: '40px'
+      }
+    },
+    nav: {
+      component: defaultNav,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: '40px'
+      }
+    },
+    leftAside: {
+      component: defaultLeftAside,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: null
+      },
+      isTopNav: true
+    },
+    rightAside: {
+      component: defaultRightAside,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: null
+      },
+      isTopFooter: true,
+      isTopHeader: true
+    },
+    footer: {
+      component: defaultFooter,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: '40px'
+      }
+    },
+    children: defaultChildren
+  }
+}`,...(x=(m=i.parameters)==null?void 0:m.docs)==null?void 0:x.source}}};var b,h,k;r.parameters={...r.parameters,docs:{...(b=r.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: args => <ProviderApp {...args} />,
+  args: {
+    defaultBgColor: 'black50',
+    isScrollOutlet: false,
+    header: {
+      component: defaultHeader,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: '40px'
+      }
+    },
+    footer: {
+      component: defaultFooter,
+      length: {
+        default: '80px',
+        tablet: '60px',
+        mobile: null
+      }
+    },
+    children: defaultChildren
+  }
+}`,...(k=(h=r.parameters)==null?void 0:h.docs)==null?void 0:k.source}}};const E=["Default","AsideDifferentTop","WithoutAsides"];export{i as AsideDifferentTop,o as Default,r as WithoutAsides,E as __namedExportsOrder,A as default};
